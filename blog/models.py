c0900ad3a53b75e6a57e8fd1823517e78611a5b6
doubletcn <thanhtrungbbox@gmail.com>
2023-05-13 13:04:04 +0700

    first commit

---
+++ blog/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+
+
+# Create your models here.
+class PublicManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    objects = models.Manager()
+    published = PublicManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
+
+
+
+
+
